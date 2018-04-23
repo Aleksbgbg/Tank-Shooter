242d8d59f18020a6dd9b3a31e4c85cede512c62b
Aleksbgbg <kozichka01@abv.bg>
2018-04-23 20:35:20 +0100

    Add 'main.js' with basic configuration.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const config = {
+    screen: {
+        width: 800,
+        height: 300
+    }
+};
+
+function setup() {
+    createCanvas(config.screen.width, config.screen.height);
+}
+
+function draw() {
+}
